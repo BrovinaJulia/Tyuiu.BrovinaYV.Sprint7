f272b8fba57da7bb9700462257e6c732f90e8a61
Юлия Бровина <Tyuiu.BrovinaYV@outlook.com>
2023-12-14 20:09:37 +0500

    Edit0

---
+++ Tyuiu.BrovinaYV.Sprint7.ProjectV11.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BrovinaYV.Sprint7.ProjectV11.Lib
+{
+    public class DataService
+    {
+    }
+}
---
+++ Tyuiu.BrovinaYV.Sprint7.ProjectV11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BrovinaYV.Sprint7.ProjectV11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BrovinaYV.Sprint7.ProjectV11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d92e8230-e474-48a3-b3b2-b4d4f893069b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
